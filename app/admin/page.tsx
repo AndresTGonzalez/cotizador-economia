07229e4991acb33df790ca0152eb7ea78c7f2865
Andrés Tapia González <andrestgonza@gmail.com>
2023-11-06 13:00:15 -0500

    Creada estructura del proyecto

---
+++ app/admin/page.tsx
@@ -0,0 +1,4 @@
+// `app/page.tsx` is the UI for the `/` URL
+export default function AdminPage() {
+  return <main>Admin</main>;
+}
---
+++ app/login/page.tsx
@@ -0,0 +1,4 @@
+// `app/page.tsx` is the UI for the `/` URL
+export default function LoginPage() {
+  return <main>Login</main>;
+}
