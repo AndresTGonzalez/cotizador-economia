a14fba080be758c37040973e330de1e3033513dd
Arocano <92830653+Arocano@users.noreply.github.com>
2023-11-09 17:13:25 -0500

    table

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,23 +1,25 @@
 "use client";
 import AdminForm from "@/components/ProductForm";
-import { Reorder } from "framer-motion";
-import Image from "next/image";
-import { Input, Button } from "@nextui-org/react";
-import { useState } from "react";
+import Drop from "@/components/dropdown";
+import { Table, TableHeader, TableColumn, TableBody, TableRow, TableCell, User, Chip, Tooltip, ChipProps, getKeyValue, Button } from "@nextui-org/react";
+import { database } from '@/app/firebase';
+import React, { useState, useEffect } from "react";
+import { collection, addDoc, doc, updateDoc, getDocs, query, onSnapshot, QuerySnapshot } from "firebase/firestore";
+import { EditIcon } from "@/icons/editIcon";
+import { DeleteIcon } from "@/icons/deleteIcon";
+import TableProducts from "@/components/TableProducts";
 
 export default function AdminPage() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
   return (
-
     <main className="flex w-screen h-screen items-center justify-center bg-neutral-100">
+      <div className="absolute top-4 right-4">
+        <Drop />
+      </div>
       <div className="w-3/4 h-4/5 bg-white rounded-xl shadow-lg flex flex-row">
         <div className="w-full h-full flex flex-col items-center justify-center rounded-r-xl">
-        <AdminForm />
+          <TableProducts/>
         </div>
       </div>
     </main>
-
-    
   );
 }
---
+++ components/dropdown.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import { Input, Button, Dropdown, DropdownItem, DropdownTrigger, DropdownMenu } from "@nextui-org/react";
+export default function Drop() {
+ 
+  return (
+    <Dropdown>
+    <DropdownTrigger>
+      <Button variant="bordered">Opciones</Button>
+    </DropdownTrigger>
+    <DropdownMenu aria-label="Link Actions">
+      <DropdownItem key="about" href="/">
+        Visualizar cotizador
+      </DropdownItem>
+      <DropdownItem key="home" href="/home">
+        Cerrar sesión
+      </DropdownItem>
+    </DropdownMenu>
+  </Dropdown>
+  );
+}
